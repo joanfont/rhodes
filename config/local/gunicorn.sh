f46b33db87818016f4f1f3fcac752d31cb2c2cf4
Joan Font <joanfont@gmail.com>
2015-03-07 21:21:01 +0100

    new config structure

---
+++ config/local/gunicorn.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+FLASKDIR=/Users/joanfont/Developer/Python/rhodes
+VIRTUAL_ENV=/Users/joanfont/.virtualenvs/rhodes
+USER=ubuntu
+GROUP=ubuntu
+LOGFILE=$FLASKDIR/log/gunicorn.log
+LOGERRFILE=$FLASKDIR/log/gunicorn_err.log
+
+LOGDIR=$(dirname $LOGFILE)
+
+NUM_WORKERS=2
+
+cd $FLASKDIR
+source $VIRTUAL_ENV/bin/activate
+
+export PYTHONPATH=$FLASKDIR:$PYTHONPATH
+
+test -d $LOGDIR || mkdir -p $LOGDIR
+
+exec $VIRTUAL_ENV/bin/gunicorn rhodes:app -w $NUM_WORKERS \
+  --user=$USER --group=$GROUP --log-level=warning \
+  --log-file=$LOGFILE -b 127.0.0.1:8080 2>> $LOGERRFILE
