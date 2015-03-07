bd7ecb87fb1c8711546992ee3f1ca2c3a5bf986e
Joan Font <joanfont@gmail.com>
2015-03-07 23:31:55 +0100

    charset dsn

--- config/local/rhodes.py
+++ config/local/rhodes.py
@@ -4,8 +4,8 @@ DB_USER = 'root'
 DB_PASS = ''
 DB_NAME = 'rhodes'
 
-DB_DSN = '{driver}://{user}:{passw}@{host}/{name}'.format(driver=DB_DRIVER,
-                                                          user=DB_USER,
-                                                          passw=DB_PASS,
-                                                          host=DB_HOST,
-                                                          name=DB_NAME)
+DB_DSN = '{driver}://{user}:{passw}@{host}/{name}?charset=utf8'.format(driver=DB_DRIVER,
+                                                                       user=DB_USER,
+                                                                       passw=DB_PASS,
+                                                                       host=DB_HOST,
+                                                                       name=DB_NAME)
