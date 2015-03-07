70bfe556ab91e603776c51267d30793d99768041
Joan Font <joanfont@gmail.com>
2015-03-07 21:30:08 +0100

    prod dsn

--- config/prod/rhodes.py
+++ config/prod/rhodes.py
@@ -2,3 +2,9 @@ DB_HOST = 'localhost'
 DB_USER = 'root'
 DB_PASS = 'gretsch'
 DB_NAME = 'rhodes'
+
+DB_DSN = '{driver}://{user}:{passw}@{host}/{name}'.format(driver=DB_DRIVER,
+                                                          user=DB_USER,
+                                                          passw=DB_PASS,
+                                                          host=DB_HOST,
+                                                          name=DB_NAME)
