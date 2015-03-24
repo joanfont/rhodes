267ffe964a9312ac6694fa94a4004fb1dd53580b
Joan Font <joanfont@gmail.com>
2015-03-24 17:05:16 +0100

    gunicorn settings

--- config/prod/gunicorn.sh
+++ config/prod/gunicorn.sh
@@ -11,6 +11,7 @@ LOGERRFILE=$FLASKDIR/log/gunicorn_err.log
 LOGDIR=$(dirname $LOGFILE)
 
 NUM_WORKERS=10
+TIMEOUT=60
 
 cd $FLASKDIR
 source $VIRTUAL_ENV/bin/activate
@@ -20,5 +21,5 @@ export PYTHONPATH=$FLASKDIR:$PYTHONPATH
 test -d $LOGDIR || mkdir -p $LOGDIR
 
 exec $VIRTUAL_ENV/bin/gunicorn rhodes:app -w $NUM_WORKERS \
-  --user=$USER --group=$GROUP --log-level=warning \
+  --user=$USER --group=$GROUP --timeout $TIMEOUT --log-level=warning \
   --log-file=$LOGFILE -b 127.0.0.1:8080 2>> $LOGERRFILE
