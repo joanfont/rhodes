8aa5e2cf428a7f8796827b772ee1b48003e39d87
Joan Font <joanfont@gmail.com>
2015-04-06 18:41:56 +0200

    fixes
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes

--- application/tasks/generate_user_auth_tokens.py
+++ application/tasks/generate_user_auth_tokens.py
@@ -1,10 +1,19 @@
+import sys
+sys.path.append('/root/rhodes/')
+
 from application.lib.models import User
 from common.session import manager
-
+from common.auth import encode_auth_token
+from common.auth import encode_password
 
 
 def generate_user_auth_token(user):
-    user.auth_token = user.generate_auth_token()
+    token = user.generate_auth_token()
+    password = user.user.lower()
+    md5passwd = encode_password(password)
+    b64t = encode_auth_token(token)
+    user.auth_token = b64t
+    user.password = md5passwd
 
 
 if __name__ == '__main__':
