66e89c25832ba599b59d2947d10260db6362ae6c
Joan Font <joanfont@gmail.com>
2015-04-09 19:06:27 +0200

    added logging

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,9 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+
 from config import config
 
 
@@ -8,3 +12,14 @@ app.config['JSON_SORT_KEYS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_DSN
 
 db = SQLAlchemy(app)
+
+formatter = logging.Formatter(
+    "[%(asctime)s] [%(pathname)s:%(lineno)d] %(levelname)s - %(message)s")
+
+file_handler = RotatingFileHandler(config.LOG_FILE, maxBytes=10000000, backupCount=5)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+
+app.logger.addHandler(file_handler)
+
+logger = app.logger
