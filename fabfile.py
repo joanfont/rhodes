001059ee68a532ca824c200f3c922049dee01bfa
Joan Font <joanfont@gmail.com>
2015-05-04 23:57:55 +0200

    hotfix in fabric script

--- fabfile.py
+++ fabfile.py
@@ -46,7 +46,6 @@ def branch(name):
 def migrate():
 
     lib_dir = os.path.join(prod_config.PROJECT_DIR, 'application/lib')
-    virtualenv()
     with cd(lib_dir):
         with virtualenv():
             run('alembic revision --autogenerate')
