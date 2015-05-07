801edb2450dad53d644579a2c7aa84ac3991a185
Joan Font <joanfont@gmail.com>
2015-05-07 15:56:07 +0200

    notification views

--- api/views/notification.py
+++ api/views/notification.py
@@ -4,6 +4,43 @@ from application.services.message import GetUserSubjectsLastMessages, GetUserGro
     GetUserConversationsLastMessages
 
 
+class MessagesNotificationsView(ListAPIViewMixin):
+
+    @auth_token_required
+    def get_action(self, *args, **kwargs):
+
+        def serialize(x):
+            return x.to_dict()
+
+        user = kwargs.get('user')
+
+        get_last_subject_messages = GetUserSubjectsLastMessages()
+        subject_messages = get_last_subject_messages.call({
+            'user_id': user.id
+        })
+
+        subject_messages_serialized = map(serialize, subject_messages)
+
+        get_last_group_messages = GetUserGroupsLastMessages()
+        group_messages = get_last_group_messages.call({
+            'user_id': user.id
+        })
+        group_messages_serialized = map(serialize, group_messages)
+
+        get_last_conversations_messages = GetUserConversationsLastMessages()
+        direct_messages = get_last_conversations_messages.call({
+            'user_id': user.id
+        })
+        direct_messages_serialized = map(serialize, direct_messages)
+
+        return {
+            'subjects': subject_messages_serialized,
+            'groups': group_messages_serialized,
+            'chats': direct_messages_serialized
+        }
+
+
+
 class SubjectMessagesNotificationsView(ListAPIViewMixin, ModelResponseMixin):
 
     @auth_token_required
