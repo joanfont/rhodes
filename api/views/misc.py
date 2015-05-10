157ecd256260b2090f23ecde6a6f1bab8d4d2543
Joan Font <joanfont@gmail.com>
2015-05-10 11:25:45 +0200

    added user types to config

--- api/views/misc.py
+++ api/views/misc.py
@@ -1,5 +1,6 @@
 from api.lib.decorators import auth_token_required
 from api.lib.mixins import ListAPIViewMixin
+from application.lib.models import UserType
 
 from config import config
 
@@ -9,6 +10,10 @@ class ConfigView(ListAPIViewMixin):
     @auth_token_required
     def get_action(self, *args, **kwargs):
         return {
-            'message_max_length': config.MESSAGE_MAX_LENGTH
+            'message_max_length': config.MESSAGE_MAX_LENGTH,
+            'user_types': {
+                UserType.TEACHER: 'Teacher',
+                UserType.STUDENT: 'Student'
+            },
         }
 
